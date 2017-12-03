5aa694c0afd724f313ffa22f720369648dc76ab7
manirox <32426583+manirox@users.noreply.github.com>
2017-12-02 18:55:55 -0600

    Update Project1.1.R

--- Project1.1.R
+++ Project1.1.R
@@ -1,3 +1,24 @@
+Diabities_data = read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data",sep = ',',quote = "\"")
+#print(housing_data_import1)
+head(Diabities_data)
+colnames(Diabities_data) = c("NTP","PGC-2","DBP","TSFT","2-HSI","BMI","DPF","AGE","CLASS")
+(Diabities_data)
+#PickupSample=sample(Diabities_data,replace = TRUE,40)
+#(PickupSample)
+write.csv(Diabities_data,"/Users/manishreddybendhi/Desktop/Fun/Rprogramming/Assigenment4DataScience/Dstaset.csv",row.names = FALSE)
+?write.csv
+varMis = c(NA,3,4,5,22,NA)
+
+sum(is.na(varMis))
+##Code to insert null values in the dta set 
+
+
+
+
+
+
+
+########Code for preprossesing of data 
 rm(list=ls())
 
 install.packages("VIM")
@@ -19,12 +40,46 @@ for(i in 1:ncol(Diabities_data1))
 boxplot(Diabities_data1)
 summary(Diabities_data1)
 plot(Diabities_data1$NTP,Diabities_data1$CLASS)
+plot(Diabities_data1$PGC.2,Diabities_data1$CLASS)
+plot(Diabities_data1$DBP,Diabities_data1$CLASS)
+plot(Diabities_data1$TSFT,Diabities_data1$CLASS)
+plot(Diabities_data1$HSI,Diabities_data1$CLASS)
+plot(Diabities_data1$BMI,Diabities_data1$CLASS)
+plot(Diabities_data1$DPF,Diabities_data1$CLASS)
+plot(Diabities_data1$AGE,Diabities_data1$CLASS)
+
+
 
 
 #######Using Knn Impute ##
 
-
+library(DMwR)
 ImputedDs = knnImputation(Diabities_data1, k = 10)
 print(ImputedDs)
 #Writing Imputed values into a new csv file 
 write.csv(ImputedDs,"/Users/manishreddybendhi/Desktop/Fun/Rprogramming/Assigenment4DataScience/ImputedValues1.csv",row.names = FALSE)
+
+CleanResult1=read.csv("/Users/manishreddybendhi/Desktop/Fun/Rprogramming/Assigenment4DataScience/ImputedValues1.csv")
+
+
+#getting Unique values in the data set 
+table(CleanResult1$CLASS)
+
+#applying  scale 
+scale(CleanResult1, center = TRUE, scale = TRUE)
+
+
+
+#DataSubsetting
+#Breaking the funciton into 5 fold cross vaidation
+
+SplittingTheData <- function()
+  
+{
+  #sampling the dta
+  Datasplit1 <- CleanResult1[sample(nrow(CleanResult1)),]
+  print(Datasplit1)
+  datafolds1 <- cut(seq(1,nrow(Datasplit1)),breaks = 5,labels = FALSE)
+  print(datafolds1)
+}
+SplittingTheData()
